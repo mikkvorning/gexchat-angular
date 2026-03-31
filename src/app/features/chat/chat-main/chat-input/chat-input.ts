c82a51f3ab601d06b73a5b1e003164ef36af602a
mikkvorning <kotk@hotmail.com>
2026-03-31 13:06:38 +0200

    - Chat input styled

--- src/app/features/chat/chat-main/chat-input/chat-input.ts
+++ src/app/features/chat/chat-main/chat-input/chat-input.ts
@@ -1,15 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatIconButton } from '@angular/material/button';
+import { MatFormField, MatSuffix } from '@angular/material/form-field';
+import { MatIcon } from '@angular/material/icon';
+import { MatInput } from '@angular/material/input';
 import { SnackbarService } from '@core/services/snackbar.service';
-import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-chat-input',
-  imports: [MatButton],
+  imports: [
+    FormsModule,
+    MatIconButton,
+    MatIconButton,
+    MatFormField,
+    MatSuffix,
+    MatInput,
+    MatIcon,
+  ],
   templateUrl: './chat-input.html',
   styleUrl: './chat-input.scss',
 })
 export class ChatInput {
   constructor(private readonly snackbar: SnackbarService) {}
+  messageValue = '';
 
   sendMessage() {
     this.snackbar.show('Message sent', 'info');
