5b0fc920a8bfbd023ed501a1d0a83770443cb7d1
mikkvorning <kotk@hotmail.com>
2026-04-06 16:58:26 +0200

    Auth routes added along with templating

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,30 @@
 import { Routes } from '@angular/router';
+import { Login } from '@features/auth/login/login';
+import { Register } from '@features/auth/register/register';
+import { Verify } from '@features/auth/verify/verify';
 import { Chat } from '@features/chat/chat';
 
 export const routes: Routes = [
+  {
+    path: 'register',
+    component: Register,
+  },
+  {
+    path: 'signup',
+    redirectTo: 'register',
+  },
+  {
+    path: 'login',
+    component: Login,
+  },
+  {
+    path: 'signin',
+    redirectTo: 'login',
+  },
+  {
+    path: 'verify',
+    component: Verify,
+  },
   {
     path: '',
     component: Chat,
